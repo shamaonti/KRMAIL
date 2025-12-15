cd7552db412810a26b07da91edd1e815608f0ab0
Jayesh Baviskar <jayeshbaviskar@earnwealth.in>
2025-12-15 15:48:47 +0530

    Update file server.js

--- server/server.js
+++ server/server.js
@@ -22,11 +22,13 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: ['http://localhost:8080', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+
+app.options('*', cors());
 
 // Compression middleware
 app.use(compression());
